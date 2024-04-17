035ab5f8dafcb501967a7707caae90995b2e1044
erfann31 <erfannasri2@gmail.com>
2024-04-17 07:23:16 +0330

    fix main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:ui_flutter_course/screens/login_signup_screen.dart';
 import 'package:ui_flutter_course/screens/home_screen.dart';
 
 void main() => runApp(
       MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: HomeScreen(),
+        home: LoginSignupScreen(showSignupTexts: false,),
         theme: ThemeData(
           scaffoldBackgroundColor: Color(0xffD6D7EF),
               primaryColor: Color(0xff9395D3),
