89016e782ebd6d54ddcb42dca29413e51871c4a3
erfann31 <erfannasri2@gmail.com>
2024-04-17 06:14:26 +0330

    fix main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:ui_flutter_course/screens/completed_screen.dart';
-import 'package:ui_flutter_course/screens/todo_screen.dart';
+import 'package:ui_flutter_course/screens/home_screen.dart';
 
-/// Flutter code sample for [BottomNavigationBar].
-
-void main() => runApp(const BottomNavigationBarExampleApp());
-
-class BottomNavigationBarExampleApp extends StatelessWidget {
-  const BottomNavigationBarExampleApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static List<Widget> _widgetOptions = <Widget>[
-    ToDoScreen(),
-    CompletedScreen()
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.format_list_bulleted_outlined),
-            label: 'All',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.check),
-            label: 'Completed',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Color(0xff9395D3),
-        onTap: _onItemTapped,
+void main() => runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen(),
+        theme: ThemeData(
+          scaffoldBackgroundColor: Color(0xffD6D7EF),
+              primaryColor: Color(0xff9395D3),
+        ),
       ),
     );
-  }
-}
