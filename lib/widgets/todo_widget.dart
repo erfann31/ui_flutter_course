af2af2cae2e05ecab7d84c4bab9d42de1d19c000
erfann31 <erfannasri2@gmail.com>
2024-04-17 06:26:43 +0330

    fix todo_widget.dart

--- lib/widgets/todo_widget.dart
+++ lib/widgets/todo_widget.dart
@@ -9,97 +9,76 @@ class TodoWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Card(
+      color: Colors.white,
+      elevation: 4,
       margin: EdgeInsets.symmetric(vertical: 10),
-      decoration: BoxDecoration(
-        color: Color(0xffffffff),
-        shape: BoxShape.rectangle,
+      shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(12.0),
       ),
       child: Padding(
         padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
           children: [
             Column(
-              mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.max,
               children: [
                 Text(
                   "TODO TITLE",
-                  textAlign: TextAlign.start,
-                  overflow: TextOverflow.clip,
                   style: TextStyle(
                     fontWeight: FontWeight.w700,
-                    fontStyle: FontStyle.normal,
                     fontSize: 18,
-                    color: Color(0xff9496d1),
+                    color: Theme.of(context).primaryColor,
                   ),
                 ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(0, 8, 0, 0),
-                  child: Text(
-                    "TODO SUB TITLE",
-                    textAlign: TextAlign.start,
-                    overflow: TextOverflow.clip,
-                    style: TextStyle(
-                      fontWeight: FontWeight.w300,
-                      fontStyle: FontStyle.normal,
-                      fontSize: 12,
-                      color: Color(0xff000000),
-                    ),
+                SizedBox(height: 8),
+                Text(
+                  "TODO SUB TITLE",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w300,
+                    fontSize: 12,
+                    color: Colors.black,
                   ),
                 ),
               ],
             ),
             if (showIconsRow)
               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisSize: MainAxisSize.max,
                 children: [
-                  GestureDetector(
-                    child: IconButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => AddEditTodoScreen(
-                              isEdit: true,
-                            ),
+                  IconButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => AddEditTodoScreen(
+                            isEdit: true,
                           ),
-                        );
-                      },
-                      icon: Icon(
-                        Icons.edit_outlined,
-                        color: Color(0xffB3B7EE),
-                        size: 24,
-                      ),
+                        ),
+                      );
+                    },
+                    icon: Icon(
+                      Icons.edit_outlined,
+                      color: Theme.of(context).primaryColor,
+                      size: 24,
                     ),
                   ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(16, 0, 0, 0),
-                    child: IconButton(
-                      onPressed: () {},
-                      icon: Icon(
-                        Icons.delete_outline,
-                        color: Color(0xffB3B7EE),
-                        size: 24,
-                      ),
+                  SizedBox(width: 16),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.delete_outline,
+                      color: Theme.of(context).primaryColor,
+                      size: 24,
                     ),
                   ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(16, 0, 0, 0),
-                    child: IconButton(
-                      onPressed: () {},
-                      icon: Icon(
-                        Icons.check_circle_outlined,
-                        color: Color(0xffB3B7EE),
-                        size: 24,
-                      ),
+                  SizedBox(width: 16),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.check_circle_outlined,
+                      color: Theme.of(context).primaryColor,
+                      size: 24,
                     ),
                   ),
                 ],
