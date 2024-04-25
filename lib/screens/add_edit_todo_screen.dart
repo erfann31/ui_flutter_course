735e9c72c8873ead3ef055f0900cc035a3782a27
erfann31 <erfannasri2@gmail.com>
2024-04-25 10:39:22 +0330

    replace MaterialButton with ElevatedButton

--- lib/screens/add_edit_todo_screen.dart
+++ lib/screens/add_edit_todo_screen.dart
@@ -47,14 +47,12 @@ class AddEditTodoScreen extends StatelessWidget {
               controller: TextEditingController(),
               style: TextStyle(
                 fontWeight: FontWeight.w400,
-                fontStyle: FontStyle.normal,
                 fontSize: 14,
               ),
               decoration: InputDecoration(
                 hintText: "Title",
                 hintStyle: TextStyle(
                   fontWeight: FontWeight.w400,
-                  fontStyle: FontStyle.normal,
                   fontSize: 14,
                   color: Colors.grey,
                 ),
@@ -67,14 +65,12 @@ class AddEditTodoScreen extends StatelessWidget {
                 controller: TextEditingController(),
                 style: TextStyle(
                   fontWeight: FontWeight.w400,
-                  fontStyle: FontStyle.normal,
                   fontSize: 14,
                 ),
                 decoration: InputDecoration(
                   hintText: "Detail",
                   hintStyle: TextStyle(
                     fontWeight: FontWeight.w400,
-                    fontStyle: FontStyle.normal,
                     fontSize: 14,
                     color: Colors.grey,
                   ),
@@ -88,68 +84,65 @@ class AddEditTodoScreen extends StatelessWidget {
                   ? Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        MaterialButton(
+                        ElevatedButton(
                           onPressed: () {},
-                          color: Theme.of(context).primaryColor,
-                          elevation: 8,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(16),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Theme.of(context).primaryColor,
+                            elevation: 8,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(16),
+                            ),
+                            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                            minimumSize: Size(175, 70),
                           ),
-                          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
                           child: Text(
                             "Update",
-                            style: TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w500,
-                            ),
+                            style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: Colors.white),
                           ),
-                          textColor: Colors.white,
-                          height: 70,
-                          minWidth: 175,
                         ),
-                        MaterialButton(
+                        ElevatedButton(
                           onPressed: () {
-                            Navigator.pop(
-                              context,
-                            );
+                            Navigator.pop(context);
                           },
-                          color: Theme.of(context).primaryColor,
-                          elevation: 8,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(16),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Theme.of(context).primaryColor,
+                            elevation: 8,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(16),
+                            ),
+                            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                            minimumSize: Size(175, 70),
                           ),
-                          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
                           child: Text(
                             "Cancel",
                             style: TextStyle(
                               fontSize: 14,
                               fontWeight: FontWeight.w500,
+                              color: Colors.white,
                             ),
                           ),
-                          textColor: Colors.white,
-                          height: 70,
-                          minWidth: 175,
-                        ),
+                        )
                       ],
                     )
-                  : MaterialButton(
+                  : ElevatedButton(
                       onPressed: () {},
-                      color: Theme.of(context).primaryColor,
-                      elevation: 8,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Theme.of(context).primaryColor,
+                        elevation: 8,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                        minimumSize: Size(double.infinity, 70),
                       ),
-                      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
                       child: Text(
                         "ADD",
                         style: TextStyle(
                           fontSize: 20,
                           fontWeight: FontWeight.w700,
+                          color: Colors.white,
                         ),
                       ),
-                      textColor: Colors.white,
-                      height: 70,
-                      minWidth: double.infinity,
                     ),
             )
           ],
