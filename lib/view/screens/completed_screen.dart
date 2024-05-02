dc93a571f9a4a26533c2db0b32350854a8f396fe
erfann31 <erfannasri2@gmail.com>
2024-05-02 18:40:19 +0330

    rewrite completed_screen

---
+++ lib/view/screens/completed_screen.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:ui_flutter_course/model/consts.dart';
+import 'package:ui_flutter_course/model/viewModel/task_viewmodel.dart';
+import 'package:ui_flutter_course/view/widgets/todo_widget.dart';
+
+class CompletedScreen extends StatefulWidget {
+  final VoidCallback onBack;
+
+  CompletedScreen({required this.onBack});
+
+  @override
+  State<CompletedScreen> createState() => _CompletedScreenState();
+}
+
+class _CompletedScreenState extends State<CompletedScreen> {
+  @override
+  void initState() {
+    fetchCompletedTasks();
+    super.initState();
+  }
+
+  Future<void> fetchCompletedTasks() async {
+    await viewModel.getCompleteTasks();
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 80,
+        backgroundColor: Theme.of(context).primaryColor,
+        title: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Text(
+            "Completed Task",
+            style: TextStyle(
+              fontWeight: FontWeight.w700,
+              fontStyle: FontStyle.normal,
+              fontSize: 24,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 12.0),
+          child: GestureDetector(
+            onTap: widget.onBack,
+            child: Icon(
+              Icons.arrow_back,
+              color: Colors.white,
+              size: 36,
+            ),
+          ),
+        ),
+      ),
+      body: viewModel.completedTasks.isEmpty // Check if completedTasks is empty
+          ? Center(child: CircularProgressIndicator()) // Show loading indicator if tasks are being fetched
+          : ListView.builder(
+              itemCount: viewModel.completedTasks.length,
+              itemBuilder: (context, index) {
+                final task = viewModel.completedTasks[index];
+                return TodoWidget(
+                  task: task,
+                  showIconsRow: false,
+                  onDelete: () {},
+                  onComplete: () {},
+                );
+              },
+            ),
+    );
+  }
+}
