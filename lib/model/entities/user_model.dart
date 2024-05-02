5b50b656bc4422709d109be2efaf08b2819330e2
erfann31 <erfannasri2@gmail.com>
2024-05-02 18:42:16 +0330

    add user_model.dart

---
+++ lib/model/entities/user_model.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+class User {
+  final String email;
+  final String? password;
+
+  User({
+    required this.email,
+    this.password,
+  });
+
+  factory User.fromRawJson(String str) => User.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory User.fromJson(Map<String, dynamic> json) => User(
+    email: json["email"],
+    password: json["password"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "email": email,
+    "password": password,
+  };
+}
