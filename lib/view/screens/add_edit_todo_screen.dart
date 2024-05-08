57b5d448d6892a986b7f19610ebf1a8b592e7820
erfann31 <erfannasri2@gmail.com>
2024-05-08 13:53:10 +0330

    fix add_edit_todo_screen.dart connect to api

--- lib/view/screens/add_edit_todo_screen.dart
+++ lib/view/screens/add_edit_todo_screen.dart
@@ -1,9 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:ui_flutter_course/model/consts.dart';
+import 'package:ui_flutter_course/model/entities/task_model.dart';
 
-class AddEditTodoScreen extends StatelessWidget {
+class AddEditTodoScreen extends StatefulWidget {
   final bool isEdit;
+  final Task? task;
+  final Function(Task task)? onCreate;
+  final Function(Task task)? onEdit;
 
-  AddEditTodoScreen({required this.isEdit});
+  AddEditTodoScreen({required this.isEdit, this.task, this.onCreate, this.onEdit});
+
+  @override
+  State<AddEditTodoScreen> createState() => _AddEditTodoScreenState();
+}
+
+class _AddEditTodoScreenState extends State<AddEditTodoScreen> {
+  final TextEditingController titleController = TextEditingController();
+  final TextEditingController descController = TextEditingController();
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    if (widget.task != null) {
+      titleController.text = widget.task!.title;
+      descController.text = widget.task!.description;
+    }
+    super.initState();
+  }
+
+  void handleNewTask() async {
+    setState(() {
+      isLoading = true;
+    });
+    if (titleController.text.trim() == "" || descController.text.trim() == "") {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Please fill all of text fields"),
+        ),
+      );
+    } else {
+      Task task = Task(title: titleController.text.trim(), description: descController.text.trim());
+      try {
+        await widget.onCreate?.call(task);
+      } catch (error) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text("An error occurred while creating a new task: $error"),
+          ),
+        );
+      } finally {
+        setState(() {
+          isLoading = false;
+        });
+      }
+    }
+  }
+
+  void handleEditTask() async {
+    setState(() {
+      isLoading = true;
+    });
+    if (titleController.text.trim() == "" || descController.text.trim() == "") {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Please fill all of text fields"),
+        ),
+      );
+    } else {
+      Task task =
+          Task(id: widget.task?.id, title: titleController.text.trim(), description: descController.text.trim());
+      try {
+        await widget.onEdit?.call(task);
+      } catch (error) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text("An error occurred while creating a new task: $error"),
+          ),
+        );
+      } finally {
+        setState(() {
+          isLoading = false;
+        });
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +96,7 @@ class AddEditTodoScreen extends StatelessWidget {
         title: Padding(
           padding: const EdgeInsets.all(12.0),
           child: Text(
-            isEdit ? "Edit Task" : "Add Task",
+            widget.isEdit ? "Edit Task" : "Add Task",
             style: TextStyle(
               fontWeight: FontWeight.w700,
               fontStyle: FontStyle.normal,
@@ -44,7 +124,7 @@ class AddEditTodoScreen extends StatelessWidget {
         child: Column(
           children: [
             TextField(
-              controller: TextEditingController(),
+              controller: titleController,
               style: TextStyle(
                 fontWeight: FontWeight.w400,
                 fontSize: 14,
@@ -62,7 +142,7 @@ class AddEditTodoScreen extends StatelessWidget {
             Padding(
               padding: EdgeInsets.only(top: 20),
               child: TextField(
-                controller: TextEditingController(),
+                controller: descController,
                 style: TextStyle(
                   fontWeight: FontWeight.w400,
                   fontSize: 14,
@@ -80,12 +160,12 @@ class AddEditTodoScreen extends StatelessWidget {
             ),
             Padding(
               padding: EdgeInsets.only(top: 40),
-              child: isEdit
+              child: widget.isEdit
                   ? Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         ElevatedButton(
-                          onPressed: () {},
+                          onPressed: handleEditTask,
                           style: ElevatedButton.styleFrom(
                             backgroundColor: Theme.of(context).primaryColor,
                             elevation: 8,
@@ -95,10 +175,14 @@ class AddEditTodoScreen extends StatelessWidget {
                             padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
                             minimumSize: Size(175, 70),
                           ),
-                          child: Text(
-                            "Update",
-                            style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: Colors.white),
-                          ),
+                          child: isLoading
+                              ? CircularProgressIndicator(
+                                  color: Colors.white,
+                                )
+                              : Text(
+                                  "Update",
+                                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: Colors.white),
+                                ),
                         ),
                         ElevatedButton(
                           onPressed: () {
@@ -125,7 +209,7 @@ class AddEditTodoScreen extends StatelessWidget {
                       ],
                     )
                   : ElevatedButton(
-                      onPressed: () {},
+                      onPressed: handleNewTask,
                       style: ElevatedButton.styleFrom(
                         backgroundColor: Theme.of(context).primaryColor,
                         elevation: 8,
@@ -135,14 +219,18 @@ class AddEditTodoScreen extends StatelessWidget {
                         padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
                         minimumSize: Size(double.infinity, 70),
                       ),
-                      child: Text(
-                        "ADD",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w700,
-                          color: Colors.white,
-                        ),
-                      ),
+                      child: isLoading
+                          ? CircularProgressIndicator(
+                              color: Colors.white,
+                            )
+                          : Text(
+                              "ADD",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w700,
+                                color: Colors.white,
+                              ),
+                            ),
                     ),
             )
           ],
