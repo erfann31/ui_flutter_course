12698895d170931a0dc9788d940449d1d14d70c4
erfann31 <erfannasri2@gmail.com>
2024-05-09 11:42:10 +0330

    make todo_widget.dart responsive

--- lib/view/widgets/todo_widget.dart
+++ lib/view/widgets/todo_widget.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:ui_flutter_course/model/consts.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:ui_flutter_course/model/entities/task_model.dart';
 import 'package:ui_flutter_course/view/screens/add_edit_todo_screen.dart';
 
 class TodoWidget extends StatefulWidget {
   final bool showIconsRow;
   final Task task;
-  final VoidCallback onDelete;
-  final VoidCallback onComplete;
+  final VoidCallback? onDelete;
+  final VoidCallback? onComplete;
   final Function(Task task)? onEdit;
-  TodoWidget(
-      {required this.showIconsRow, required this.task, required this.onDelete, required this.onComplete, this.onEdit});
+  TodoWidget({
+    required this.showIconsRow,
+    required this.task,
+    this.onDelete,
+    this.onComplete,
+    this.onEdit,
+  });
 
   @override
   State<TodoWidget> createState() => _TodoWidgetState();
@@ -19,47 +24,51 @@ class TodoWidget extends StatefulWidget {
 class _TodoWidgetState extends State<TodoWidget> {
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {},
-      child: Container(
-        margin: EdgeInsets.symmetric(vertical: 10),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(16),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.4),
-              blurRadius: 4,
-              offset: Offset(0, 2),
-            ),
-          ],
-        ),
-        child: Padding(
-          padding: EdgeInsets.all(20),
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10.w),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16.r),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.4),
+            blurRadius: 4,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(20.r),
+        child: SizedBox(
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    widget.task.title,
-                    style: TextStyle(
-                      fontWeight: FontWeight.w700,
-                      fontSize: 17,
-                      color: Theme.of(context).primaryColor,
+              SizedBox(
+                width: 180.w,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      overflow: TextOverflow.ellipsis,
+                      widget.task.title,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 17.sp,
+                        color: Theme.of(context).primaryColor,
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    widget.task.description,
-                    style: TextStyle(
-                      fontWeight: FontWeight.w400,
-                      fontSize: 12,
-                      color: Colors.black,
+                    SizedBox(height: 8.h),
+                    Text(
+                      overflow: TextOverflow.ellipsis,
+                      widget.task.description,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 12.sp,
+                        color: Colors.black,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
               if (widget.showIconsRow)
                 Row(
@@ -83,7 +92,7 @@ class _TodoWidgetState extends State<TodoWidget> {
                       icon: Icon(
                         Icons.edit_outlined,
                         color: Theme.of(context).primaryColor,
-                        size: 24,
+                        size: 24.sp,
                       ),
                     ),
                     IconButton(
@@ -91,7 +100,7 @@ class _TodoWidgetState extends State<TodoWidget> {
                       icon: Icon(
                         Icons.delete_outline,
                         color: Theme.of(context).primaryColor,
-                        size: 24,
+                        size: 24.sp,
                       ),
                     ),
                     IconButton(
@@ -99,7 +108,7 @@ class _TodoWidgetState extends State<TodoWidget> {
                       icon: Icon(
                         Icons.check_circle_outlined,
                         color: Theme.of(context).primaryColor,
-                        size: 24,
+                        size: 24.sp,
                       ),
                     ),
                   ],
