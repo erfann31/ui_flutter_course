fe6db911ff132f7e4623eaebb7fc58ed53b798d4
erfann31 <erfannasri2@gmail.com>
2024-05-09 11:41:04 +0330

    add screen util to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:ui_flutter_course/view/screens/login_signup_screen.dart';
 
 void main() => runApp(
-      MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: LoginSignupScreen(isSignup: false,),
-        theme: ThemeData(
-          scaffoldBackgroundColor: Color(0xffD6D7EF),
-              primaryColor: Color(0xff9395D3),
+  ScreenUtilInit(
+    minTextAdapt: true,
+    splitScreenMode: true,
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: LoginSignupScreen(isSignup: false,),
+          theme: ThemeData(
+            scaffoldBackgroundColor: Color(0xffD6D7EF),
+                primaryColor: Color(0xff9395D3),
+          ),
         ),
       ),
     );
